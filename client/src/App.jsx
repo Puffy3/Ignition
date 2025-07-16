596c7cec15897ef4fffe2dd042ca171e0613690c
prasun <prasun.1928@gmail.com>
2025-07-16 05:32:00 +0530

    add folder structure

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,36 @@
 
 import './App.css'
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function App() {
-  
+  const [message, setMessage] = useState('');
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get('http://localhost:5001/api/test', {
+          responseType: 'text' 
+        });
+        setMessage(response.data);
+      } catch (error) {
+        setMessage('Error fetching data');
+        console.error(error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
-    <>
-    Ignition
-    </>
-  )
+    <div>
+      <h1>Message from server:</h1>
+      <p>{message}</p>
+    </div>
+  );
 }
 
-export default App
+export default App;
+
+
+
